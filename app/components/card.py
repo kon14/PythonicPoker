30da251381c1081628c5d1d217ec980b3361a661
Konstantinos Feretos <konferetos@tutanota.com>
2025-03-10 17:52:25 +0200

    fix(app): Card component variant mapping

--- app/components/card.py
+++ app/components/card.py
@@ -11,26 +11,27 @@ CARD_DIMENSIONS = (60, 84)
 SPRITE_ASSET_ROOT = os.path.join(ASSET_ROOT, "img", "cards")
 
 class CardStateEnum(Enum):
-    Visible = 0
-    Hidden = 1
-    Discarded = 2
+    Visible = "visible_card"
+    Hidden = "hidden_card"
+    Discarded = "discarded_card"
 
 
 class Card:
     def __init__(self, card: GameState.MatchState.MatchStatePlayerPublicInfo.HandCard):
         self.card: Optional[ProtoCard] = None
-        if card.visible_card:
+        card_variant = card.WhichOneof("card")
+        if card_variant == CardStateEnum.Visible.value:
             self.state = CardStateEnum.Visible
             self.card = card.visible_card
             card_rank = ProtoCard.CardRank.Name(card.visible_card.rank)
             card_suit = ProtoCard.CardSuit.Name(card.visible_card.suit)
             sprite_name = f"{card_rank.lower()}_{card_suit.lower()}"
-        elif card.hidden_card:
+        elif card_variant == CardStateEnum.Hidden.value:
             self.state = CardStateEnum.Hidden
             sprite_name = f"back_blue_basic"
-        elif card.discarded_card:
+        elif card_variant == CardStateEnum.Discarded.value:
             self.state = CardStateEnum.Discarded
-            sprite_name = f"blank_front_with_num_borders"
+            sprite_name = f"blank_front_with_num_boarders"
         sprite_path = os.path.join(SPRITE_ASSET_ROOT, f"{sprite_name}_white.png")
         self.sprite = pygame.image.load(sprite_path)
 
